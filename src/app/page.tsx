dba6ea62924f74a5a932e329067e963829a7bed6
felipescunha <fcunha.ti@gmail.com>
2024-06-30 16:53:31 -0300

    feat: :sparkles: start project

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="w-screen h-screen flex justify-center">
+      <h1>Hello World!</h1>
+    </div>
+  );
+}
