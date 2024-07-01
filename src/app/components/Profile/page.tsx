24a6d11f85a3b1c57ce57d07bc5920bc853e189d
felipescunha <fcunha.ti@gmail.com>
2024-06-30 22:01:19 -0300

    feat: :sparkles: profile component

---
+++ src/app/components/Profile/page.tsx
@@ -0,0 +1,111 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+import Image from "next/image";
+import {
+  AlertDialog,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+
+interface ProfileProps {
+  avatar: string | undefined;
+  name: string;
+  username: string;
+  bio?: string;
+  github: string;
+  blog?: string | undefined;
+  company?: string;
+  location?: string;
+  public_repos: number;
+  followers: number;
+  following: number;
+}
+
+const Profile: React.FC<ProfileProps> = ({
+  avatar,
+  name,
+  username,
+  bio,
+  github,
+  blog,
+  company,
+  location,
+  public_repos,
+  followers,
+  following,
+}) => {
+  return (
+    <div className="p-4 flex flex-col justify-start items-center max-w-xl pb-2">
+      <div className="mt-3 justify-center items-center ">
+        <Image
+          src={avatar ? avatar : "/avatar.png"}
+          alt="Avatar"
+          width={256}
+          height={256}
+          className="rounded-full mt-2"
+        />
+      </div>
+      <div className="flex flex-col mt-2 text-left">
+        <p className="text-xl text-white font-medium">{name}</p>
+        <span className=" text-gray-200">{username}</span>
+        <div className="text-white bg-gray-700 rounded-md p-2 mt-2 text-center">
+          <AlertDialog>
+            <AlertDialogTrigger>Clique para ver a bio</AlertDialogTrigger>
+            <AlertDialogContent>
+              <AlertDialogHeader>
+                <AlertDialogTitle>Biografia</AlertDialogTitle>
+                <AlertDialogDescription>
+                  {bio ? bio : "Este usuário não possui bio :("}
+                </AlertDialogDescription>
+              </AlertDialogHeader>
+              <AlertDialogFooter>
+                <AlertDialogCancel>Cancelar</AlertDialogCancel>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
+        </div>
+        <div className="flex flex-row text-sm mt-3 mb-1">
+          <Image src="icon-users.svg" alt="users" width={16} height={16} />
+          <span className="text-white font-medium mr-1 ">{followers}</span>
+          <p className="text-gray-200"> followers </p>
+          <span className="text-white font-medium ml-2 mr-2 "> · </span>
+          <span className="text-white font-medium">{following}</span>
+        <p className="text-gray-200">following</p>
+        </div>
+        {blog ? (
+          <Link href={blog} className="text-white font-medium text-sm">
+            {blog}
+          </Link>
+        ) : null}
+        {github ? (
+          <Link href={github} className="text-white font-medium text-sm">
+            {github}
+          </Link>
+        ) : null}
+        <div className="flex fle-row mt-2">
+          {location ? (
+            <Image
+              src="icon-location.svg"
+              alt="Location"
+              width={16}
+              height={16}
+            />
+          ) : null}
+          <p className="text-gray-200 text-sm">{location}</p>
+        </div>
+        <p className="text-gray-200 text-sm">{company}</p>
+        <p className="text-gray-200 text-sm">
+          Public repositories: {public_repos}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,30 @@
 "use client";
 import React, { useState } from "react";
+import { IProfile } from "@/interfaces/profile.interface";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
+import { ToastMessage } from "@/lib/toastMessages";
+import Profile from "./components/Profile/page";
+import api from "@/services/api";
 
 export default function Home() {
   const [username, setUsername] = useState<string>("");
+  const [profile, setProfile] = useState<IProfile | null>(null);
+
+  const handleGetProfile = async (userVariable?: string) => {
+    console.log("aqui")
+    try {
+      const response = await api.get(
+        `github/users/${userVariable === undefined ? username : userVariable}`
+      );
+      setProfile(response.data);
+    } catch (error) {
+      ToastMessage("Usuário não encontrado");
+      setProfile(null);
+      setUsername("");
+    }
+  };
 
   function handleClearProfile() {
     setUsername("");
@@ -16,7 +35,23 @@ export default function Home() {
     <div className="flex flex-row justify-center items-center h-screen w-screen">
       <div className="flex justify-center items-center">
         <div className="">
-          
+        {profile != null ? (
+              <Profile
+                avatar={profile?.avatar_url}
+                name={profile?.name}
+                username={profile?.login}
+                bio={profile?.bio}
+                github={profile?.html_url}
+                public_repos={profile?.public_repos}
+                location={profile?.location}
+                followers={profile?.followers}
+                following={profile?.following}
+                company={profile?.company}
+                blog={profile?.blog} 
+              />
+            ) : (
+              <div></div>
+            )}
           <div className="flex flex-col gap-2.5 justify-center pl-4 pr-4 w-96">
             <Input
               type="text"
@@ -26,6 +61,9 @@ export default function Home() {
               className="w-full px-3 py-2 border-transparent focus:border-transparent focus:ring-0"
             />
             <Button variant="outline"
+             onClick={() => {
+              handleGetProfile();
+            }}
             >
               Buscar usuário
             </Button>
